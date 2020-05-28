76ae779ba215a6aba183b0ca009e0819de08e8fc
hayato <8810ino3@gmail.com>
2020-05-28 23:49:48 +0900

    関数化しました。

--- main.js
+++ main.js
@@ -1,41 +1,53 @@
 'use strict'
 
-const table = document.getElementById('table');
+const tbody = document.getElementById('tbody');
 const addTask = document.getElementById('add-task');
-let i = 1;
 const todos = [];
-
-
-document.getElementById('add-button').addEventListener('click', () =>{
-  const workButton = document.createElement('input');
-  workButton.type = 'button';
-  workButton.value = '作業中'
-  const deleteButton = document.createElement('input');
-  deleteButton.type = 'button';
-  deleteButton.value = '削除'
-
+function todosPush(){
   const todo = {
     task:addTask.value,
-    work:workButton.value,
-    deldet:deleteButton.value
+    work:'作業中'
   }
-  
   todos.push(todo);
-  console.log(todos);
+  return;
+}
+function createWorkButton(workButton){
+  workButton.type = 'button';
+  workButton.value = '作業中'
+  return;
+}
+function createDeleteButton(deleteButton){
+  deleteButton.type = 'button';
+  deleteButton.value = '削除'
+  return;
+}
+function tbodyAddTodo(workButton,deleteButton){
+  //パーツを作り変数化する
   const tr = document.createElement('tr');
-  const tdI = document.createElement('td')
+  const tdLength = document.createElement('td')
   const tdAddTask = document.createElement('td');
   const workingTd = document.createElement('td');
   workingTd.appendChild(workButton);
   const deleteTd = document.createElement('td');
   deleteTd.appendChild(deleteButton);
-  
-  tdI.textContent = i;
+  //パーツを表示する
+  tdLength.textContent = todos.length;
   tdAddTask.textContent = addTask.value;
-  tr.appendChild(tdI);
+  tr.appendChild(tdLength);
   tr.appendChild(tdAddTask);
   tr.appendChild(workingTd);
   tr.appendChild(deleteTd);
-  table.appendChild(tr);
-  i++;
+  tbody.appendChild(tr);
+
+  return;
+}
+
+
+document.getElementById('add-button').addEventListener('click', () =>{
+  const workButton = document.createElement('input');
+  createWorkButton(workButton);
+  const deleteButton = document.createElement('input');
+  createDeleteButton(deleteButton);
+  todosPush();
+  tbodyAddTodo(workButton,deleteButton);
 })
